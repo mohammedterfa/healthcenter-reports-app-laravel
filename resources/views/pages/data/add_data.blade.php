a2e8d451a427017492f9b08f5954bcd9a0977703
mohammedterfa <mohammedterfa@gmail.com>
2023-09-24 17:25:39 +0300

    fix some desgin issue

--- resources/views/pages/data/add_data.blade.php
+++ resources/views/pages/data/add_data.blade.php
@@ -10,20 +10,20 @@
                     <h2 class="font-semibold text-slate-800 dark:text-slate-100">إدخال البيانات</h2>
                 </header>
                 <div id="dashboard-card-04-legend" class="px-5 py-3 flex justify-center">
-                    <form>
+                    <form class="sm:w-1/2 w-full">
                         <div class="mb-6">
                           <label for="name" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">إسم المريض</label>
-                          <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 w-96 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                          <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
                         </div>
                         <div class="mb-6">
                           <label for="disease" class="block mb-2 text-lg  font-medium text-gray-900 dark:text-white">المرض</label>
-                          <input type="text" id="disease" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                          <input type="text" id="disease" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
                         <div class="mb-6">
                             <label for="date" class="block mb-2 text-lg  font-medium text-gray-900 dark:text-white">التاريخ</label>
-                            <input type="date" id="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                            <input type="date" id="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                           </div>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">حفظ</button>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">حفظ</button>
                       </form>
                 </div>
 
